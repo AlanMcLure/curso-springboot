b1ba9edeba18b904928c507c28df20864450519c
AlanMcLure <alanmclurealarcon02@gmail.com>
2024-03-26 20:34:31 +0100

    mini ejemplo api rest customer

---
+++ cursoSpringBoot/src/main/java/cursoSpringBoot/controllers/CustomerController.java
@@ -0,0 +1,26 @@
+package cursoSpringBoot.controllers;
+
+import cursoSpringBoot.domain.Customer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class CustomerController {
+
+    private List<Customer> customers = new ArrayList<Customer>(Arrays.asList(
+            new Customer(123, "Alan McLure", "alanmclure", "contrasena123"),
+            new Customer(456, "Alejandra García", "alegarcia", "clave456"),
+            new Customer(789, "Laura Sanchéz", "lauras", "secreto789"),
+            new Customer(234, "Carlos Martínez", "carlosm", "password234")
+    ));
+
+    @GetMapping("clientes")
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+}
